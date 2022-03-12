5bc5a15bc053c5e0b5d0b214bca4a1d694778cb5
Kai Chiang <kelichiang@gmail.com>
2022-03-12 22:46:58 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,115 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Giraffe)
+    basic.showLeds(`
+        # . . . #
+        # . . . .
+        # . . . .
+        # # # . .
+        # . # . .
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . . . #
+        . . . . #
+        # # . . #
+        . # . . #
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . . # .
+        . . . # .
+        # . . # #
+        # . . # .
+        `)
+    basic.showLeds(`
+        . # # . .
+        . . # . .
+        . . # . .
+        . . # # #
+        . . # . #
+        `)
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        . # . . .
+        . # # # .
+        . # . # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Giraffe)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # # # .
+        . # . # .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . # # # .
+        . # . # .
+        # # . . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        # # . . .
+        . # . . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # . . .
+        . # . . .
+        . # . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        . # . . .
+        . # # # .
+        . # . # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Giraffe)
+    basic.showLeds(`
+        . # # . .
+        . . # . .
+        . . # . .
+        . . # # #
+        . . # . #
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . . # .
+        . . . # .
+        # . . # #
+        # . . # .
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . . . #
+        . . . . #
+        # # . . #
+        . # . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        # . . . .
+        # . . . .
+        # # # . .
+        # . # . .
+        `)
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        . # . . .
+        . # # # .
+        . # . # .
+        `)
+})
+basic.showIcon(IconNames.Giraffe)
